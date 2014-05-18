da3063327be9db8e2b63afd90f639aa53aed764b
Paul Ruiz <paultr@sportslabs.com>
2014-05-17 20:30:25 -0600

    added google plus authentication

--- challenge2/src/main/java/com/govdevchallenge/team11/challenge2/activities/AuthActivity.java
+++ challenge2/src/main/java/com/govdevchallenge/team11/challenge2/activities/AuthActivity.java
@@ -2,33 +2,168 @@ package com.govdevchallenge.team11.challenge2.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 import com.govdevchallenge.team11.challenge2.R;
+
+import com.google.android.gms.common.ConnectionResult;
+
 
 /**
  * Created by PaulTR on 5/17/14.
  */
-public class AuthActivity extends Activity {
+public class AuthActivity extends Activity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
-	SignInButton mSignInButton;
+	private static final int RC_SIGN_IN = 0;
+	// Logcat tag
+	private static final String TAG = "MainActivity";
+
+	// Profile pic image size in pixels
+	private static final int PROFILE_PIC_SIZE = 400;
+
+	// Google client to interact with Google API
+	private GoogleApiClient mGoogleApiClient;
+
+	/**
+	 * A flag indicating that a PendingIntent is in progress and prevents us
+	 * from starting further intents.
+	 */
+	private boolean mIntentInProgress;
+
+	private boolean mSignInClicked;
+
+	private ConnectionResult mConnectionResult;
+
+	private SignInButton mSigninButton;
+	private Button btnSignOut, btnRevokeAccess;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView( R.layout.activity_auth );
+		setContentView(R.layout.activity_auth);
 
-		mSignInButton = (SignInButton) findViewById( R.id.sign_in_button );
+		mSigninButton = (SignInButton) findViewById( R.id.sign_in_button );
+		mSigninButton.setOnClickListener(this);
 
-		mSignInButton.setOnClickListener( new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				Intent intent = new Intent( getApplicationContext(), MainActivity.class );
-				startActivity( intent );
-				finish();
-			}
-		});
+		// Initializing google plus api client
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+				.addConnectionCallbacks(this)
+				.addOnConnectionFailedListener(this).addApi(Plus.API, null)
+				.addScope(Plus.SCOPE_PLUS_LOGIN).build();
 	}
+
+	protected void onStart() {
+		super.onStart();
+		mGoogleApiClient.connect();
+	}
+
+	protected void onStop() {
+		super.onStop();
+		if (mGoogleApiClient.isConnected()) {
+			mGoogleApiClient.disconnect();
+		}
+	}
+
+	/**
+	 * Button on click listener
+	 * */
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.sign_in_button:
+				// Signin button clicked
+				signInWithGplus();
+		}
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!result.hasResolution()) {
+			GooglePlayServicesUtil.getErrorDialog(result.getErrorCode(), this,
+					0).show();
+			return;
+		}
+
+		if (!mIntentInProgress) {
+			// Store the ConnectionResult for later usage
+			mConnectionResult = result;
+
+			if (mSignInClicked) {
+				// The user has already clicked 'sign-in' so we attempt to
+				// resolve all
+				// errors until the user is signed in, or they cancel.
+				resolveSignInError();
+			}
+		}
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int responseCode,
+									Intent intent) {
+		if (requestCode == RC_SIGN_IN) {
+			if (responseCode != RESULT_OK) {
+				mSignInClicked = false;
+			}
+
+			mIntentInProgress = false;
+
+			if (!mGoogleApiClient.isConnecting()) {
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		mSignInClicked = false;
+		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+		onAuthenticated();
+
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		mGoogleApiClient.connect();
+	}
+
+	/**
+	 * Sign-in into google
+	 * */
+	private void signInWithGplus() {
+		if (!mGoogleApiClient.isConnecting()) {
+			mSignInClicked = true;
+			resolveSignInError();
+		}
+	}
+
+	/**
+	 * Method to resolve any signin errors
+	 * */
+	private void resolveSignInError() {
+		if (mConnectionResult.hasResolution()) {
+			try {
+				mIntentInProgress = true;
+				mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+			} catch (IntentSender.SendIntentException e) {
+				mIntentInProgress = false;
+				mGoogleApiClient.connect();
+			}
+		}
+	}
+
+	private void onAuthenticated() {
+		Intent intent = new Intent( getApplicationContext(), MainActivity.class );
+		startActivity( intent );
+		finish();
+	}
+
 }
